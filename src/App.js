a3eb9671e48f52772d7cafe7047bb315378a9989
Masum Mollo <freelancermasummolla@gmail.com>
2022-04-26 23:04:41 +0600

    send user date to server

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
-import AddUser from './components/AddUser/AddUser';
 import Home from './components/Home/Home';
+import Adduser from './components/AddUser/Adduser';
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path='/' element={<Home></Home>}></Route>
-        <Route path='user/add' element={<AddUser></AddUser>}></Route>
+        <Route path='user/add' element={<Adduser></Adduser>}></Route>
       </Routes>
     </div>
   );
--- src/components/AddUser/AddUser.js
+++ src/components/AddUser/AddUser.js
@@ -1,11 +1,37 @@
 import React from 'react';
+import './Adduser.css';
 
-const AddUser = () => {
+const Adduser = () => {
+    const handleAddUser = event =>{
+        event.preventDefault();
+        const name = event.target.name.value;
+        const email = event.target.email.value;
+
+        const user = {name, email}
+
+        // Send data to the server
+        fetch('http://localhost:6001/user', {
+            method: 'POST',
+            headers:{
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(user)
+        })
+        .then(res => res.json())
+        .then(data =>{console.log('Success', data)})
+    }
     return (
         <div>
-            <h2>Add a new user</h2>
+            <h1>Please add a new user</h1>
+            <form onSubmit={handleAddUser}>
+                <input className='btn' type="text" name="name" id="" placeholder='Name' required/>
+                <br />
+                <input className='btn' type="email" name="email" id="" placeholder='Email' required/>
+                <br />
+                <input className='btnSubmit' type="submit" value="Add new user" />
+            </form>
         </div>
     );
 };
 
-export default AddUser;
+export default Adduser;
