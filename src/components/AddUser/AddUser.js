efb540f61138f8d7c2095cc2e4354b950dd2e898
Masum Mollo <freelancermasummolla@gmail.com>
2022-04-29 12:41:27 +0600

    add user event

--- src/components/AddUser/AddUser.js
+++ src/components/AddUser/AddUser.js
@@ -1,7 +1,28 @@
 import React from 'react';
 import './AddUser.css'
 const AddUser = () => {
-    
+    const handleAddUser = event =>{
+        event.preventDefault();
+        const name = event.target.name.value;
+        const email = event.target.email.value;
+
+        const user = {name, email};
+
+        // send data to the server
+        fetch('http://localhost:6001/user', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(user)
+        })
+        .then(res => res.json())
+        .then(data =>{
+            console.log('success', data);
+            alert('users added successfully!');
+            event.target.reset();
+        })
+    }
     return (
         <div>
             <h2>Please add a new User</h2>
